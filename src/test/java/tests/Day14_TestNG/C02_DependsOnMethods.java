8fe143011539e36414777fcd480111d2590a9e8f
hasan cebe <hasancebe237@gmail.com>
2023-01-14 13:59:14 +0300

    140123

--- src/test/java/tests/Day14_TestNG/C02_DependsOnMethods.java
+++ src/test/java/tests/Day14_TestNG/C02_DependsOnMethods.java
@@ -1,5 +1,20 @@
 package tests.Day14_TestNG;
 
-public class C02_DependsOnMethods {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class C02_DependsOnMethods {
+    @Test
+    public void firstTest(){
+        System.out.println("first test running");
+        Assert.assertTrue(5>9);
+    }
+    @Test (dependsOnMethods = "firstTest")
+    public void secondTest(){
+        System.out.println("second test running");
+    }
+    @Test
+    public void thirdTest(){
+        System.out.println("third test running");
+    }
 }
---
+++ src/test/java/tests/Day14_TestNG/C03_NutellaTestWithTestNG.java
@@ -0,0 +1,17 @@
+package tests.Day14_TestNG;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C03_NutellaTestWithTestNG {
+    @Test
+    public void test01() {
+        Driver.getDriver().get("https://amazon.com");
+        WebElement searchBox=Driver.getDriver().findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("Nutella");
+
+     Driver.closeDriver();
+    }
+}
