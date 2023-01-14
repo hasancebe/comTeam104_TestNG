57bfb5024c11145f975ac60fa175b82326eb330c
hasan cebe <hasancebe237@gmail.com>
2023-01-14 13:30:46 +0300

    firstadding

---
+++ src/test/java/tests/Day14_TestNG/C01_TestNG_FirstTest.java
@@ -0,0 +1,20 @@
+package tests.Day14_TestNG;
+
+import org.testng.annotations.Test;
+//priority sayısı küçük olan önce çaılşır.
+// eğer eşitse alfabatik olarak küçük  önce çalışır
+// priority değeri yazılmazsa default olarak '0' kabul edilir.
+public class C01_TestNG_FirstTest {
+    @Test (priority = 7)
+    public void firstTest(){
+        System.out.println("first test running");
+    }
+    @Test (priority = 2)
+    public void secondTest(){
+        System.out.println("second test running");
+    }
+    @Test (priority = 44)
+    public void thirdTest(){
+        System.out.println("third test running");
+    }
+}
